8c0cdebf7884de8e4c33bcbb85b6b8d0b301a789
Luca Montana <lucamontana89@gmail.com>
2022-01-06 18:05:32 +0100

    Cut sequences HL and HS
    Sequences 615 bp for HL, and 234 for HS. Now creates dataset with both Seq615 and Seq234

--- Script 0 - Prepare sequences haplo library.R
+++ Script 0 - Prepare sequences haplo library.R
@@ -24,46 +24,41 @@ library(msa)
 #library(ape)
 
 # Data --------------------------------------------------------------------
-# Multiple sequence alignment ---------------------------------------------
 
-## Upload databases: originally in ACCESS folder on Drive
+## Upload databases: originally in ACCESS folder on Drive -----------------
 d <- read_excel("~/Documents/Post-Docs/IML/MOBELS/dloop/DB/ACCESS/20211216_MOBELS.xlsx", sheet = "D-Loop", na = "NA")  # database in ACCESS folder on Drive
 s <- read_excel("~/Documents/Post-Docs/IML/MOBELS/dloop/DB/ACCESS/20211216_MOBELS.xlsx", sheet = "Specimens", na = "NA")
 
-## Subset columns
-# Dloop
-#d <- data.frame(d)
+## Format input database for MSA ------------------------------------------
+# Dloop database
 str(d)
 colnames(d)[3] <- "Numero_unique_extrait"
 d <- subset(d, Qualite_sequence==1, select = c(Numero_unique_specimen, Numero_unique_extrait, Qualite_sequence, Sequence_consensus, N_nucl))
 d <- d[!is.na(d$Sequence_consensus),]
 table(d$Qualite_sequence)
 
-# Specimens
-#s <- data.frame(s)
+# Specimens databse
 str(s)
-s <- subset(s, select = c(Numero_unique_specimen, Nom_commun, Age, Sexe_visuel, Region_echantillonnage, Annee_echantillonnage:Jour_echantillonnage))
+s <- s[,c("Numero_unique_specimen","Nom_commun","Age","Sexe_visuel","Region_echantillonnage","Annee_echantillonnage","Mois_echantillonnage","Jour_echantillonnage")]
 
 # Merge and remove narwhals and hybrids
 dt <- merge(d, s, by = "Numero_unique_specimen")  # excludes specimens that are in s but not in d (18 specimens, nrow = 3641)
 table(dt$Nom_commun)
-dt <- subset(dt, Nom_commun == "Beluga", select = c(Numero_unique_specimen, Numero_unique_extrait, Sequence_consensus, N_nucl, Age:Jour_echantillonnage))
+dt <- dt[dt$Nom_commun %in% "Beluga", c("Numero_unique_specimen","Numero_unique_extrait","Sequence_consensus","N_nucl","Age","Sexe_visuel",
+                                        "Region_echantillonnage","Annee_echantillonnage","Mois_echantillonnage","Jour_echantillonnage")]
 
-## Format Sequence_consensus
+### Format Sequence_consensus ---------------------------------------------
 dt$Sequence_consensus <- toupper(dt$Sequence_consensus)
 dt$Sequence_consensus <- gsub("-", "", dt$Sequence_consensus)
 dt$N_nucl2 <- nchar(dt$Sequence_consensus)
+
+# Verify which ambiguities are included in sequences at present
 nt <- paste(dt$Sequence_consensus, collapse = "")
 table(strsplit(nt, split = ""))
 #     A      C      D      G      H      K      M      N      R      S      T      W      Y 
 #630308 515581      1 336738      1     20     19      1     11     10 708704     41     21
-# Don't remember what I wanted to do here... Interesting way the for loop to count for number of ambiguities in 1_Database...
-#n <- data.frame(matrix(NA, nrow = nrow(dt), ncol = 16))
-#colnames(n) <- c("Numero_unique_specimen","A","C","T","G","N","R","Y","K","M","S","W","B","D","H","V")
-#for(i in 1:nrow(dt)){
-#}
 
-## Identify duplicated sequences
+### Identify duplicated sequences -----------------------------------------
 dt$Duplicated <- NA
 uni <- dt[!duplicated(dt$Numero_unique_specimen),]
 dup <- dt[duplicated(dt$Numero_unique_specimen),]
@@ -75,6 +70,8 @@ dup2 <- dup2[!duplicated(dup2$Numero_unique_specimen),]
 dup2$Duplicated <- 3
 dup3$Duplicated <- 4
 dloop <- rbind(uni,dup,dup2,dup3)
+
+# Add identifier in Numero_unique_specimen
 dloop$Numero_unique_specimen2 <- ifelse(is.na(dloop$Duplicated), dloop$Numero_unique_specimen,
                                         paste(dloop$Numero_unique_specimen, dloop$Duplicated, sep = "-"))
 dloop <- subset(dloop, select = c(Numero_unique_specimen2, Numero_unique_extrait, Sequence_consensus, N_nucl, N_nucl2, Age:Jour_echantillonnage))
@@ -83,14 +80,15 @@ dloop <- arrange(dloop, Numero_unique_specimen)
 
 
 # Multiple sequence alignment ---------------------------------------------
-# Prepare DNAStringSet object
+
+## Prepare DNAStringSet object --------------------------------------------
 seq <- dloop$Sequence_consensus
 #table(strsplit(seq[1], split = ""))  # verify which nucleotides or ambiguities are present (maybe do it for all sequence with a final summary table?)
-dna <- DNAStringSet(seq, use.names = T)
+dna <- DNAStringSet(seq)
 names(dna) <- dloop$Numero_unique_specimen
 #writeXStringSet(dna, "Beluga_n3284.fasta")
 
-# Seq in revcomplement found in Mega
+## Seq in revcomplement found in Mega -------------------------------------
 # S_20_00647 (duplicated), 01198, 01618, 01638, 02908, 03180, 03202
 dna$S_20_00647 <- reverseComplement(dna$S_20_00647)
 dna$S_20_01198 <- reverseComplement(dna$S_20_01198)
@@ -101,7 +99,7 @@ dna$S_20_03180 <- reverseComplement(dna$S_20_03180)
 dna$S_20_03202 <- reverseComplement(dna$S_20_03202)
 writeXStringSet(dna, "Beluga_complete_n3284.fasta")
 
-# Alignment
+## MSA --------------------------------------------------------------------
 dna.algn <- msa(dna, method = "Muscle", gapOpening = 10000, gapExten = 400, maxiters = 30, type = "dna",
                 order = "input", verbose = T)
 print(dna.algn, show = "complete")  # 87 to 701+1
@@ -115,31 +113,69 @@ alignment <- DNAStringSet(dna.algn)  # to save the alignment
 writeXStringSet(alignment, "Beluga_alignment_complete_n3284.fasta")
 #dna.algn <- readDNAStringSet("Beluga_alignment_complete_n3284.fasta")  # upload complete alignment
 
-# Subset sequences
-# Use bp strings at start and end of consensus sequence to define the cutting loci.
-# Define F and R 'primers
-cs <- readDNAStringSet(filepath = "~/Documents/Post-Docs/IML/MOBELS/dloop/Fasta/Ref sequence/Sequence_Dloop_Ref_615pb_consensus.fasta")
-cs
-dloop.F <- "ACTACGTCAGTATTAAATAAA"
-dloop.R <- "GCTGGACCTGTGTGTATTTTT"
 
-# Define cutting position
-res.dloop.F <- vmatchPattern(DNAString(dloop.F), DNAStringSet(dna.algn), max.mismatch = 4)
-cut.dloop.F.int <- res.dloop.F@ends %>% unlist() %>% table() %>% dimnames(.)
-cut.dloop.F <- as.numeric(as.character(cut.dloop.F.int[[1]])) - nchar(dloop.F) + 1
+# Cut sequences -----------------------------------------------------------
+# Use bp strings at start and end of consensus sequences (615bp and 234bp) to define the cutting loci
 
-res.dloop.R <- vmatchPattern(DNAString(dloop.R), DNAStringSet(dna.algn), max.mismatch = 4)
-cut.dloop.R.int <- res.dloop.R@ends %>% unlist() %>% table() %>% dimnames(.)
-cut.dloop.R <- as.numeric(as.character(cut.dloop.R.int[[1]]))
+## Cut sequences 615 bp ---------------------------------------------------
 
-Dloop <- subseq(DNAStringSet(dna.algn), start = cut.dloop.F, end = cut.dloop.R)
-print(Dloop, show = "complete")
-table(Dloop@ranges@width)
-Dloop
-s615 <- Dloop
-names(s615) <- names(Dloop)
-writeXStringSet(s615, "Beluga_615bp_n3284.fasta")
-#s615 <- readDNAStringSet("Beluga_615bp_n3284.fasta")  # upload 615bp alignment
+### Define F and R 'primers' ----------------------------------------------
+cr615 <- readDNAStringSet(filepath = "~/Documents/Post-Docs/IML/MOBELS/dloop/Fasta/Ref sequence/Sequence_Dloop_Ref_615pb_consensus.fasta")
+cr615
+F615 <- "ACTACGTCAGTATTAAATAAA"  # at the beginning of 5'-end of CR (nt = 38)
+R615 <- "GCTGGACCTGTGTGTATTTTT"
+
+### Define cutting position -----------------------------------------------
+res.F615 <- vmatchPattern(DNAString(F615), DNAStringSet(dna.algn), max.mismatch = 4)
+cut.F615.int <- res.F615@ends %>% unlist() %>% table() %>% dimnames(.)
+cut.F615 <- as.numeric(as.character(cut.F615.int[[1]])) - nchar(F615) + 1
+
+res.R615 <- vmatchPattern(DNAString(R615), DNAStringSet(dna.algn), max.mismatch = 4)
+cut.R615.int <- res.R615@ends %>% unlist() %>% table() %>% dimnames(.)
+cut.R615 <- as.numeric(as.character(cut.R615.int[[1]]))
+
+### Cut sequences to 615 bp and save fasta ---------------------------------
+Dloop615 <- subseq(DNAStringSet(dna.algn), start = cut.F615, end = cut.R615)
+print(Dloop615, show = "complete")
+table(Dloop615@ranges@width)
+Dloop615
+writeXStringSet(Dloop615, "Beluga_615bp_n3284.fasta")
+#Dloop615 <- readDNAStringSet("Beluga_615bp_n3284.fasta")  # upload 615bp alignment
+
+## Cut sequences 234 bp ---------------------------------------------------
+
+### Define F and R 'primers' ----------------------------------------------
+# In the 235 basepairs routinely sequenced (position 134 to 384) we have found 18 variable sites
+# expressing 37 haplotypes in 450 individuals. FROM Lillie et al. 1996.
+# Nineteen polymorphic nucleotide positions were found and nearly all changes were transitions
+# [first SNP = 129; last SNP = 355]. FROM Brown Gladden et al. 1997.
+# The mtDNA locus we used consists of 234 nucleotides that are found at the beginning of the d-loop
+# region of the molecule (Brown Gladden et al. 1997). FROM De March & Postma 2003.
+# Primer 5'-3' used in Brown Gladden et al. and De March & Postma:
+# Bel5 <- "ACATTTTACTGTGACTATTG"  # at the beginning of 5'-end of CR (nt = 71)
+cr917 <- readDNAStringSet(filepath = "~/Documents/Post-Docs/IML/MOBELS/dloop/Fasta/Ref sequence/Sequence_Dloop_complete.fasta")
+cr917 <- as.character(cr917$`U18117.1 Delphinapterus leucas mitochondrion control region, complete sequence`)
+F234 <- substring(cr917, first = 126, last = 146)  # A few nt before the first SNP found by Brown Gladden et al . 1997
+R234 <- substring(cr917, first = 339, last = 359)  # A few nt after the last SNP found by Brown Gladden et al . 1997
+
+### Define cutting position -----------------------------------------------
+res.F234 <- vmatchPattern(DNAString(F234), DNAStringSet(dna.algn), max.mismatch = 2)
+cut.F234.int <- res.F234@ends %>% unlist() %>% table() %>% dimnames(.)
+cut.F234 <- as.numeric(as.character(cut.F234.int[[1]])) - nchar(F234) + 1 # nt 915: normal as alignment starts BEFORE the 5'-strand of CR
+
+res.R234 <- vmatchPattern(DNAString(R234), DNAStringSet(dna.algn), max.mismatch = 4)
+cut.R234.int <- res.R234@ends %>% unlist() %>% table() %>% dimnames(.)
+cut.R234 <- as.numeric(as.character(cut.R234.int[[1]]))
+
+### Cut sequences to 234 bp and save fasta ---------------------------------
+Dloop234 <- subseq(DNAStringSet(dna.algn), start = cut.F234, end = cut.R234)
+print(Dloop234, show = "complete")
+table(Dloop234@ranges@width)
+Dloop234
+writeXStringSet(Dloop234, "Beluga_234bp_n3284.fasta")
+#Dloop234 <- readDNAStringSet("Beluga_234bp_n3284.fasta")  # upload 615bp alignment
+
+
 
 
 # Add metadata to DNAStringSet and save dataset ---------------------------
