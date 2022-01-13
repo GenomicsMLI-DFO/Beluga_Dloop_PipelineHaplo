b6322dc8281bfd0c6163ee5b9cc874fddbb5b008
Luca Montana <lucamontana89@gmail.com>
2022-01-13 17:10:53 -0500

    New input data and assign haplo only to specimens with seq quality = 1

--- Script 3a - Assign haplo to individuals 615 bp.R
+++ Script 3a - Assign haplo to individuals 615 bp.R
@@ -9,38 +9,56 @@ getwd()
 rm(list = ls())
 
 # Libraries and function --------------------------------------------------
+library(readxl)
 
 
 # Data --------------------------------------------------------------------
-data <- read.csv("Sequences_Dloop615_n3283.csv")
+data <- read.csv("Sequences_Dloop615_n3314.csv")
 str(data)
 # data includes info on quality of sequences (columns N.nucl, N.ATCG, N.ambog, N.manquants) as well as if sequences is usable
 # all made in Script 2 - Compiling d_haplotype library.R
 
 
 # Assign haplotype to each individuals ------------------------------------
-lib <- read.csv('librairie_136_haplotypes615.csv') # most recent haplotype library
+lib <- read.csv('librairie_137_haplotypes615.csv') # most recent haplotype library
 colnames(lib) <- c("hapl","seq")  # if it's not already the case
 
-# Upload info on minimal sequence
+## Upload info on minimal sequence ----------------------------------------
 min_seq <- read.csv("polymorphismes_et_seq_minimale.csv", stringsAsFactors = F)  # table made in script 1
 seq_start <- as.numeric(unlist(strsplit(min_seq$Bornes.sequence.minimale[1], " - "))[1])  # start of minimal sequence
 seq_stop <- as.numeric(unlist(strsplit(min_seq$Bornes.sequence.minimale[1], " - "))[2])  # end of minimal sequence
 
-# Assign haplotype to each individual
+## Upload info Qualite_sequence -------------------------------------------
+# 0 unusable
+# 1 usable without heterozygous nucleotides
+# 2 usable with heterozygous nucleotides
+# 3 incomplete
+# 4 doubts on vaility of sequence
+# 11 good sequence in other extraction
+d <- read_excel("~/Documents/Post-Docs/IML/MOBELS/dloop/DB/ACCESS/20220112_MOBELS.xlsx", sheet = "D-Loop", na = "NA")
+colnames(d)[2] <- "Numero_unique_extrait"
+data <- merge(data, d[,c("Numero_unique_extrait","Qualite_sequence")], by = "Numero_unique_extrait")
+table(data$Qualite_sequence[data$seq_utilisable=="no"])
+# 1  2  3 11 
+#19 13  4  5
+table(data$Qualite_sequence[data$seq_utilisable=="yes"])
+#   1    2    3    4 
+#3267    4    1    1
+
+## Assign haplotype to each individual ------------------------------------
 hapind <- data.frame(matrix(ncol=1, nrow=0))
 colnames(hapind) <- c("haplotype")
 
 sequences <- toupper(data$seq)
 for (i in 1:length(sequences)){
-    if(data$seq_utilisable[i] == "yes"){
+    if(data$seq_utilisable[i] == "yes" & data$Qualite_sequence[i]==1) {
         if(substr(data$seq[i], seq_start, seq_stop) %in% substr(lib$seq, seq_start, seq_stop)) {
             hapind[i,1] <- lib$hapl[which(substr(lib$seq, seq_start, seq_stop) == substr(data$seq[i], seq_start, seq_stop))]
         } else {
             # shows if haplotype is new relative to library: you MUST generate new haplo library if this is the case
             hapind[i,1] <- "unknown haplotype"
         }
-    }else{
+    } else {
         # generates NAs for unusable sequences, to avoid confusion
         hapind[i,1] <- "NA"
     }
@@ -49,5 +67,5 @@ for (i in 1:length(sequences)){
 table(hapind)
 data2 <- cbind(data, hapind)
 
-write.csv(data2, "Dloop_haplo615_n3283.csv", row.names=F)
+write.csv(data2, "Dloop_haplo615_n3314.csv", row.names=F)
 
