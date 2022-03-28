ad8787d0f105e3f55458ddb3c427eb883ca6317a
Luca Montana <lucamontana89@gmail.com>
2022-03-28 11:40:43 -0400

    New script: verify if any issue is introduced

---
+++ Bonus_CheckPossibleIssues.R
@@ -0,0 +1,12 @@
+# Info --------------------------------------------------------------------
+# 
+# Author: Luca Montana
+# Affiliation: Fisheries and Oceans Canada (DFO)
+# Group: Genomic laboratory, Demersal and Benthic Sciences Branch 
+# Location: Institut Maurice Lamontagne 
+# Date: 2022-03-28
+# 
+# Overview: Verify if you introduce any possible mistake in the D-Loop dataset after running the pipeline
+# Qualitative check
+# 
+#
